312127b60d04c84fec5527beb72120a2bbd5bc73
rezafadillah07 <117989069+rezafadillah07@users.noreply.github.com>
2023-01-06 13:44:47 +0700

    Add files via upload

---
+++ reza01.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rezajava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class reza01  {
+    public static void main(String[] args) {
+
+    
+   sc = new Scanner(System.in);
+   reza01 person = new reza01(
+   System.out.println main(string][] args) {
+   person.toString().compareTo(string reza01)
+   System.err.print(string);
+   person.height.reza01 }
+   
+           
+           
+  
+           
+        
+    
+        
+    
+   
+   
+   
+            
+    
+   
+           
+           
+           
+            
+           
+            
+         
+        
+         
+           
+   
+//TODO code appliecation logic here 
+
+
